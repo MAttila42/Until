288c5fd64fb328cd88a97f51a9ff11eb5a3b0fe0
ExAtom <menyhart.atika@gmail.com>
2022-02-22 11:57:44 +0100

    Skiasharp test

--- Commands/Test.cs
+++ Commands/Test.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using SkiaSharp;
 using Discord;
 using Discord.Interactions;
 using Until.Services;
@@ -16,6 +20,33 @@ namespace Until.Commands
         {
             await RespondAsync(Context.User.Mention, allowedMentions: AllowedMentions.None);
             await Context.Channel.SendMessageAsync($"ping||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||{Context.User.Mention}");
+
+            string[] urls = new string[2]
+            {
+                "https://cdn.discordapp.com/attachments/932549944705970186/940878880980488192/noun-info-1228105.png",
+                "https://cdn.discordapp.com/attachments/932549944705970186/940878875716632626/noun_Close_1984788.png"
+            };
+
+            List<SKBitmap> images = new List<SKBitmap>();
+            foreach (string u in urls)
+                images.Add(SKBitmap.Decode(new MemoryStream(await new System.Net.Http.HttpClient().GetByteArrayAsync(u))));
+
+            SKSurface tempSurface = SKSurface.Create(new SKImageInfo(images.Max(i => i.Width), images.Sum(i => i.Height)));
+            SKCanvas canvas = tempSurface.Canvas;
+            canvas.Clear(SKColors.Transparent);
+
+            int offset = 0;
+            foreach (SKBitmap i in images)
+            {
+                canvas.DrawBitmap(i, SKRect.Create(0, offset, i.Width, i.Height));
+                offset += i.Height;
+            }
+
+            FileAttachment file = new FileAttachment(tempSurface.Snapshot().Encode(SKEncodedImageFormat.Png, 100).AsStream(), "asd");
+            await Context.Channel.SendFileAsync(file);
+
+            foreach (SKBitmap i in images)
+                i.Dispose();
         }
     }
 }
